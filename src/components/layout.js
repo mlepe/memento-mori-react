777788cdca6ba3e48bad0cea1afba2a4efa80cf6
mlepe <you@example.com>
2023-06-09 10:09:35 +0400

    Implemented Layout component - Created form to get user input, then passed to he Weeks component - Added Header and Footer components to the Layout render - Added basic CSS in appropriate file ("./header.css")

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,3 @@
-import { Weeks } from "./week";
-
 /*
  * File: layout.js
  * Project: memento-mori-react
@@ -11,10 +9,66 @@ import { Weeks } from "./week";
  * -----
  * Copyright 2021  - 2023 Matthieu LEPERLIER, Nomad Solutions
  */
+import React from "react";
+import { useState } from "react";
+import { Weeks } from "./week";
+import Header from "./header";
+import Footer from "./footer";
+import "./layout.css";
+import { useEffect } from "react";
 const Layout = () => {
+  const [dob, setDob] = useState();
+  const [expectancy, setExpectancy] = useState("80");
+
+  const getDob = (e) => {
+    console.group("Layout.getDob()");
+    let val = e;
+    setDob(val);
+    console.log("setDob to:", val);
+    console.groupEnd();
+  };
+
+  const getExpectancy = (e) => {
+    console.group("Layout.getExpectancy()");
+    let val = e;
+    setExpectancy(val);
+    console.log("setExpectancy to:", val);
+    console.groupEnd();
+  };
+
   return (
     <div className="App">
-      <Weeks dob="05-02-1995" expectancy={80}></Weeks>
+      <Header></Header>
+      <form className="Form">
+        <div className="Form__input">
+          <label>
+            Please enter your birthdate:<pre>&nbsp;</pre>
+          </label>
+          <input
+            name="dobInput"
+            type="date"
+            onChange={(e) => getDob(e.target.value)}
+          ></input>
+        </div>
+        <hr />
+        <div className="Form__input">
+          <label>
+            Set your life expectancy:<pre>&nbsp;</pre>
+          </label>
+          <input
+            name="expectancyInput"
+            type="number"
+            min="0"
+            //defaultValue="80"
+            max="120"
+            //onLoad={(e) => getExpectancy(e.target.value)}
+            onChange={(e) => getExpectancy(e.target.value)}
+          ></input>
+        </div>
+      </form>
+      {dob && <Weeks dob={dob} expectancy={expectancy}></Weeks>}
+      {/* <Weeks dob="05-02-1995" expectancy={80}></Weeks> */}
+      <Footer></Footer>
     </div>
   );
 };
